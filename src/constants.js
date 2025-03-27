cb5e247a2260756fcf022d150e4b1e6976e92422
santiagosommer <s.sommerg@gmail.com>
2025-03-27 11:12:08 -0300

    Interactive portfolio

---
+++ src/constants.js
@@ -0,0 +1,18 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    pc: `This is my PC. I am currently working and learning the following: Python, Go, C++, React, PostgreSQL. It's such a challenge!
+    Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/santiagosommer" target="_blank">Github</a>!`,
+    "english-certificate": `This is my english certificate. I hung it on the wall because I'm proud of it. It was so hard to do the listening part with many cars honking the horn.`,
+    "sofa-table": `That's my sofa. I like to relax here.`,
+    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">ThePrimeTimeagen</a>, <a href="https://www.youtube.com/@midudev" target="_blank">Midudev</a>, <a href="https://www.youtube.com/@Fireship" target="_blank">Fireship</a>, to give some examples`,
+    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+    resume: `This is my desk and on it is my resume. <a href="santiago_sommer_cv.pdf" target="_blank">Check it out?</a>
+  Contact me at s.sommerg@gmail.com if you have any interesting job opportunities!`,
+    projects: `About this portfolio : I'm not really into developing videogames, but I wanted something more interactive to show off my work!`,
+    library: `There are a lot of programming books on my shelves. There is even one in German (My german knowledge is very poor, I still can't read a book by myself haha).
+    I've been reading Introduction to Algorithms lately.`,
+    exit: `If you want to exit Santiago's portfolio, just close the tab.`,
+};
